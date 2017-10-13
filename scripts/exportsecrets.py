fceb2791c9639c9bc87c54e58c53b129c44669c5
erikvdbergh <erikvdbergh@gmail.com>
2017-10-13 08:48:59 +0000

    change to support yaml kube config

--- scripts/exportsecrets.py
+++ scripts/exportsecrets.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
-import json
+import yaml
 import os
 
-config = json.load(open(os.path.join(os.environ["HOME"], '.kube/config')))
+config = yaml.safe_load(open(os.path.join(os.environ["HOME"], '.kube/config')))
 
 print('export KUBE_CA='+config['clusters'][0]['cluster']['certificate-authority-data'])
 print('export KUBE_CC='+config['users'][0]['user']['client-certificate-data'])
