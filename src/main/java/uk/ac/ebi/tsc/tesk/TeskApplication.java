f69456041046d9cb2e8d0ca1cab85fe8be52cd19
Ania Niewielska <aniewielska@windows.ebi.ac.uk>
2017-11-07 13:48:18 +0000

    Initial commit

---
+++ src/main/java/uk/ac/ebi/tsc/tesk/TeskApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.ebi.tsc.tesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeskApplication.class, args);
+	}
+}
---
+++ src/test/java/uk/ac/ebi/tsc/tesk/TeskApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.ac.ebi.tsc.tesk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
