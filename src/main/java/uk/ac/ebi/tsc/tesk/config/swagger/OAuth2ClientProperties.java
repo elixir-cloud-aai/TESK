6c9951174a927f590974963dbf348bf1781f19b7
aniewielska <aniewielska@ebi.ac.uk>
2018-05-02 11:53:28 +0100

    SwaggerUI as OAuth2 client

---
+++ src/main/java/uk/ac/ebi/tsc/tesk/config/swagger/OAuth2ClientProperties.java
@@ -0,0 +1,92 @@
+package uk.ac.ebi.tsc.tesk.config.swagger;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Ania Niewielska <aniewielska@ebi.ac.uk>
+ *
+ * Properties for SwaggerUI acting as OAuth2 client
+ */
+@Configuration
+@ConfigurationProperties(prefix = "tesk.api.swagger-oauth")
+public class OAuth2ClientProperties {
+    /**
+     * Authorisation server authorisation endpoint
+     */
+    private String authorizationEndpoint;
+
+    /**
+     * Authorisation server token endpoint
+     */
+    private String tokenEndpoint;
+    /**
+     * OAuth2 Client ID
+     */
+    private String clientId;
+
+    /**
+     * OAuth2 Client Secret
+     */
+    private String clientSecret;
+
+    /**
+     * List of Scopes, a client will request (a list of entries: scope name: description)
+     */
+    private List<String> scopes;
+
+    public String getAuthorizationEndpoint() {
+        return authorizationEndpoint;
+    }
+
+    public void setAuthorizationEndpoint(String authorizationEndpoint) {
+        this.authorizationEndpoint = authorizationEndpoint;
+    }
+
+    public String getTokenEndpoint() {
+        return tokenEndpoint;
+    }
+
+    public void setTokenEndpoint(String tokenEndpoint) {
+        this.tokenEndpoint = tokenEndpoint;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+
+    public List<String> getScopes() {
+        return scopes;
+    }
+    public Map<String, String> getScopesAsMap() {
+        Map<String, String> map = new LinkedHashMap<>();
+        if (scopes == null)
+            return map;
+        for (String scope: scopes) {
+            String[] pair = StringUtils.split(scope, ":");
+            map.put(pair[0], pair[1]);
+        }
+        return map;
+    }
+
+    public void setScopes(List<String> scopes) {
+        this.scopes = scopes;
+    }
+}
---
+++ src/main/java/uk/ac/ebi/tsc/tesk/config/swagger/SwaggerConfig.java
@@ -0,0 +1,94 @@
+package uk.ac.ebi.tsc.tesk.config.swagger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.OAuthBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Swagger Configuration
+ * Configures securing all endpoints with OAuth2 (swaggerUI acts as OAuh2 client)
+ *
+ * @author Ania Niewielska <aniewielska@ebi.ac.uk>
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Autowired
+    private OAuth2ClientProperties properties;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("uk.ac.ebi.tsc.tesk.api"))
+                .paths(PathSelectors.any())
+                .build()
+                .securitySchemes(Collections.singletonList(oauth()))
+                .securityContexts(Arrays.asList(securityContext()));
+    }
+
+    @Bean
+    protected List<GrantType> grantTypes() {
+        List<GrantType> grantTypes = new ArrayList<>();
+        TokenRequestEndpoint tokenRequestEndpoint = new TokenRequestEndpoint(properties.getAuthorizationEndpoint(), properties.getClientId(), properties.getClientSecret() );
+        TokenEndpoint tokenEndpoint = new TokenEndpoint(properties.getTokenEndpoint(), "access_token");
+        grantTypes.add(new AuthorizationCodeGrant(tokenRequestEndpoint, tokenEndpoint));
+        /*LoginEndpoint loginEndpoint = new LoginEndpoint(properties.getAuthorizationEndpoint());
+        grantTypes.add(new ImplicitGrant(loginEndpoint, "access_token"));*/
+        return grantTypes;
+    }
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("oauth2", authorizationScopes));
+    }
+
+    @Bean
+    SecurityScheme oauth() {
+        return new OAuthBuilder()
+                .name("oauth2")
+                .scopes(scopes())
+                .grantTypes(grantTypes())
+                .build();
+    }
+
+    private List<AuthorizationScope> scopes() {
+        List<AuthorizationScope> list = new ArrayList();
+        properties.getScopesAsMap().forEach((name, description) -> list.add(new AuthorizationScope(name, description)));
+        return list;
+    }
+
+    @Bean
+    public SecurityConfiguration securityInfo() {
+        return SecurityConfigurationBuilder.builder()
+                .clientId(properties.getClientId())
+                .clientSecret(properties.getClientSecret())
+                .scopeSeparator("")
+                .build();
+        //return new SecurityConfiguration(clientId, clientSecret, "realm", clientId, "Bearer", ApiKeyVehicle.HEADER, "Authorization", "");
+    }
+
+}
