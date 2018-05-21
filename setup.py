10767722f9639893a557b78ae9b31131b29d08ce
erikvdbergh <erikvdbergh@gmail.com>
2018-05-21 17:56:49 +0100

    add transput filer file

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
     scripts=[
         'tesk_core/taskmaster.py',
         'tesk_core/filer.py',
+        'tesk_core/transput_filer.py',
         'tesk_core/filer_class.py',
         'tesk_core/pvc.py',
         'tesk_core/job.py'
