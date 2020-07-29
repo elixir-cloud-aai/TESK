76a5ead6fd44007315462541c6999dab1a5c3ca6
Kostas Liosis <liosis.kc@gmail.com>
2020-07-29 19:11:35 +0300

    utility improvement
    Improvements for the extract_endpoint utility

--- src/tesk_core/extract_endpoint.py
+++ src/tesk_core/extract_endpoint.py
@@ -10,7 +10,11 @@ def extract_endpoint(profile="default"):
     # Path to the config generated from secret is hardcoded
     if "AWS_CONFIG_FILE" in os.environ:
         config.read(os.environ["AWS_CONFIG_FILE"])
-        return config[profile]["endpoint_url"]
 
-    else:
-        return None
+    # Check for a config file in the default location
+    elif os.path.exists("~/.aws/config"):
+        config.read("~/.aws/config")
+
+    # Takes care of both non-existent profile and/or field
+    return config[profile]["endpoint_url"] \
+        if config.has_option(profile, "endpoint_url") else None
