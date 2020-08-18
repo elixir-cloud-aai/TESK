b8d60fd6af63e1b492f773e57bb5a213a433f6fa
aniewielska <aniewielska@ebi.ac.uk>
2020-08-18 18:22:31 +0100

    Fix UnboundLocalError

--- src/tesk_core/filer.py
+++ src/tesk_core/filer.py
@@ -305,6 +305,7 @@ class FTPTransput(Transput):
 
 
 def ftp_login(ftp_connection, netloc, netrc_file):
+    user = None
     if netrc_file is not None:
         creds = netrc_file.authenticators(netloc)
         if creds:
