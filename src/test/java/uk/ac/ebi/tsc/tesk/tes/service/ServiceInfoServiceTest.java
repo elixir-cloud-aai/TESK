703d48e8003e3793dcc3bc5c0a270a7972ddb97f
aniewielska <aniewielska@ebi.ac.uk>
2021-04-09 18:29:19 +0100

    Service info test

---
+++ src/test/java/uk/ac/ebi/tsc/tesk/tes/service/ServiceInfoServiceTest.java
@@ -0,0 +1,38 @@
+package uk.ac.ebi.tsc.tesk.tes.service;
+
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import uk.ac.ebi.tsc.tesk.tes.model.ServiceOrganization;
+import uk.ac.ebi.tsc.tesk.tes.model.TesServiceInfo;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author aniewielska
+ * @since 09/04/2021
+ */
+public class ServiceInfoServiceTest {
+
+    private Resource serviceInfoFile = new ClassPathResource("service-info.yaml");
+    private ServiceInfoService subject = new ServiceInfoService(serviceInfoFile);
+
+    @Test
+    public void getServiceInfo() throws URISyntaxException {
+        subject.setUp();
+        TesServiceInfo result = subject.getServiceInfo();
+        TesServiceInfo expected = new TesServiceInfo()
+                .name("example application")
+                .createdAt(LocalDateTime.parse("2001-12-14T21:59:00").atOffset(ZoneOffset.UTC))
+                .organization(new ServiceOrganization().name("EBI").url(new URI("http://example.com")));
+        assertThat(result, is(expected));
+        subject.loadYaml();
+        assertThat(result, is(expected));
+    }
+}
