e4dc1addfa610dab3a9129b94d67fceaf544ef23
Javed Habib <jh4official@gmail.com>
2024-07-03 09:47:28 +0530

    remove error handling

--- tesk/app.py
+++ tesk/app.py
@@ -2,8 +2,6 @@
 
 import logging
 
-from werkzeug.exceptions import InternalServerError
-
 from tesk.tesk_app import TeskApp
 
 logger = logging.getLogger(__name__)
@@ -11,13 +9,7 @@ logger = logging.getLogger(__name__)
 
 def main() -> None:
     """Run FOCA application."""
-    try:
-        TeskApp().run()
-    except Exception as error:
-        logger.exception("An error occurred while running the application.")
-        raise InternalServerError(
-            "An error occurred while running the application."
-        ) from error
+    TeskApp().run()
 
 
 if __name__ == "__main__":
