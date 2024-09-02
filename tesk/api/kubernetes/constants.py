b611cab1ea4340bfaafdcae4ad26fee673eea087
Javed Habib <jh4official@gmail.com>
2024-09-02 10:50:47 +0530

    uppercase

--- tesk/api/kubernetes/constants.py
+++ tesk/api/kubernetes/constants.py
@@ -10,47 +10,47 @@ class JobConstants(BaseModel):
     """Constants related to job and tasks.
 
     Attributes:
-        taskmaster_input: str = ENV var that serves as taskmaster script input (JSON
+        TASKMASTER_INPUT: str = ENV var that serves as taskmaster script input (JSON
             format)
-        taskmaster_input_exec_key: str = Key in JSON taskmaster input, which holds list
+        TASKMASTER_INPUT_EXEC_KEY: str = Key in JSON taskmaster input, which holds list
             of executors
-        volume_name: str = Volume name
-        job_create_attempts_no: int = Number of attempts of job creation in case of name
+        VOLUME_NAME: str = Volume name
+        JOB_CREATE_ATTEMPTS_NO: int = Number of attempts of job creation in case of name
             collision
-        job_name_taskm_prefix: str = Constant prefix of taskmaster's job name (== TES
+        JOB_NAME_TASKM_PREFIX: str = Constant prefix of taskmaster's job name (== TES
             task ID)
-        job_name_exec_prefix: str = Part of executor's job name, that follows
+        JOB_NAME_EXEC_PREFIX: str = Part of executor's job name, that follows
             taskmaster's name
-        job_name_taskm_rand_part_length: int = No of bytes of random part of task
+        JOB_NAME_TASKM_RAND_PART_LENGTH: int = No of bytes of random part of task
             master's name (which end up encoded to hex)
-        job_name_exec_no_length: int = No of digits reserved for executor number in
+        JOB_NAME_EXEC_NO_LENGTH: int = No of digits reserved for executor number in
             executor's job name. Ends up padded with '0' for numbers < 10
-        job_name_filer_suf: str = Output filer name suffix
-        resource_disk_default: float = Default resource disk value
-        completed_states: Set[str] = TES task states, indicating task is not running and
+        JOB_NAME_FILER_SUF: str = Output filer name suffix
+        RESOURCE_DISK_DEFAULT: float = Default resource disk value
+        COMPLETED_STATES: Set[str] = TES task states, indicating task is not running and
             cannot be cancelled
-        executor_backoff_limit: str = Set a number of retries of a job execution.
-        filer_backoff_limit: str = Set a number of retries of a filer job execution.
+        EXECUTOR_BACKOFF_LIMIT: str = Set a number of retries of a job execution.
+        FILER_BACKOFF_LIMIT: str = Set a number of retries of a filer job execution.
     """
 
-    taskmaster_input: str = "JSON_INPUT"
-    taskmaster_input_exec_key: str = "executors"
-    volume_name: str = "PVC"
-    job_create_attempts_no: int = 5
-    job_name_taskm_prefix: str = "task-"
-    job_name_exec_prefix: str = "-ex-"
-    job_name_taskm_rand_part_length: int = 4
-    job_name_exec_no_length: int = (2,)
-    job_name_filer_suf: str = "-outputs-filer"
-    resource_disk_default: float = 0.1
-    completed_states: Set[str] = {
+    TASKMASTER_INPUT: str = "JSON_INPUT"
+    TASKMASTER_INPUT_EXEC_KEY: str = "executors"
+    VOLUME_NAME: str = "PVC"
+    JOB_CREATE_ATTEMPTS_NO: int = 5
+    JOB_NAME_TASKM_PREFIX: str = "task-"
+    JOB_NAME_EXEC_PREFIX: str = "-ex-"
+    JOB_NAME_TASKM_RAND_PART_LENGTH: int = 4
+    JOB_NAME_EXEC_NO_LENGTH: int = (2,)
+    JOB_NAME_FILER_SUF: str = "-outputs-filer"
+    RESOURCE_DISK_DEFAULT: float = 0.1
+    COMPLETED_STATES: Set[str] = {
         "CANCELED",
         "COMPLETE",
         "EXECUTOR_ERROR",
         "SYSTEM_ERROR",
     }
-    executor_backoff_limit: str = "EXECUTOR_BACKOFF_LIMIT"
-    filer_backoff_limit: str = "FILER_BACKOFF_LIMIT"
+    EXECUTOR_BACKOFF_LIMIT: str = "EXECUTOR_BACKOFF_LIMIT"
+    FILER_BACKOFF_LIMIT: str = "FILER_BACKOFF_LIMIT"
 
     class Config:
         """Configuration for class."""
@@ -62,14 +62,14 @@ class AnnotationConstants(BaseModel):
     """Constants related to Kubernetes annotations.
 
     Attributes:
-        ann_testask_name_key: str = Key of the annotation, that stores name of TES task
+        ANN_TESTASK_NAME_KEY: str = Key of the annotation, that stores name of TES task
             in both taskmaster's job and executor's jobs
-        ann_json_input_key: str = Key of the annotation, that stores whole input TES
+        ANN_JSON_INPUT_KEY: str = Key of the annotation, that stores whole input TES
             task serialized to JSON
     """
 
-    ann_testask_name_key: str = "tes-task-name"
-    ann_json_input_key: str = "json-input"
+    ANN_TESTASK_NAME_KEY: str = "tes-task-name"
+    ANN_JSON_INPUT_KEY: str = "json-input"
 
     class Config:
         """Configuration for class."""
@@ -81,30 +81,30 @@ class LabelConstants(BaseModel):
     """Constants related to Kubernetes labels.
 
     Attributes:
-        label_testask_id_key: str = Key of the label, that stores taskmaster's name (==
+        LABEL_TESTASK_ID_KEY: str = Key of the label, that stores taskmaster's name (==
             TES task generated ID) in executor jobs
-        label_jobtype_key: str = Key of the label, that stores type of a job (taskmaster
+        LABEL_JOBTYPE_KEY: str = Key of the label, that stores type of a job (taskmaster
             or executor)
-        label_jobtype_value_taskm: str = Value of the label with taskmaster's job type
-        label_jobtype_value_exec: str = Value of the label with executor's job type
-        label_execno_key: str = Key of the label, that holds executor number for
+        LABEL_JOBTYPE_VALUE_TASKM: str = Value of the label with taskmaster's job type
+        LABEL_JOBTYPE_VALUE_EXEC: str = Value of the label with executor's job type
+        LABEL_EXECNO_KEY: str = Key of the label, that holds executor number for
             executor jobs
-        label_taskstate_key: str = Key of the label, that holds executor's state
-        label_taskstate_value_canc: str = Value of the label, that holds executor's
+        LABEL_TASKSTATE_KEY: str = Key of the label, that holds executor's state
+        LABEL_TASKSTATE_VALUE_CANC: str = Value of the label, that holds executor's
             Cancelled state
-        label_userid_key: str = Key of the label, that holds user id
-        label_groupname_key: str = Key of the label, that holds user's group name
+        LABEL_USERID_KEY: str = Key of the label, that holds user id
+        LABEL_GROUPNAME_KEY: str = Key of the label, that holds user's group name
     """
 
-    label_testask_id_key: str = "taskmaster-name"
-    label_jobtype_key: str = "job-type"
-    label_jobtype_value_taskm: str = "taskmaster"
-    label_jobtype_value_exec: str = "executor"
-    label_execno_key: str = "executor-no"
-    label_taskstate_key: str = "task-status"
-    label_taskstate_value_canc: str = "Cancelled"
-    label_userid_key: str = "creator-user-id"
-    label_groupname_key: str = "creator-group-name"
+    LABEL_TESTASK_ID_KEY: str = "taskmaster-name"
+    LABEL_JOBTYPE_KEY: str = "job-type"
+    LABEL_JOBTYPE_VALUE_TASKM: str = "taskmaster"
+    LABEL_JOBTYPE_VALUE_EXEC: str = "executor"
+    LABEL_EXECNO_KEY: str = "executor-no"
+    LABEL_TASKSTATE_KEY: str = "task-status"
+    LABEL_TASKSTATE_VALUE_CANC: str = "Cancelled"
+    LABEL_USERID_KEY: str = "creator-user-id"
+    LABEL_GROUPNAME_KEY: str = "creator-group-name"
 
     class Config:
         """Configuration for class."""
@@ -116,13 +116,13 @@ class PathValidationConstants(BaseModel):
     """Constants related to path validation.
 
     Attributes:
-        absolute_path_regex: str = Pattern to validate path
-        absolute_path_message: str = Message for absolute path validation (to avoid
+        ABSOLUTE_PATH_REGEX: str = Pattern to validate path
+        ABSOLUTE_PATH_MESSAGE: str = Message for absolute path validation (to avoid
             message.property)
     """
 
-    absolute_path_regexp: str = "^\\/.*"
-    absolute_path_message: str = "must be an absolute path"
+    ABSOLUTE_PATH_REGEXP: str = "^\\/.*"
+    ABSOLUTE_PATH_MESSAGE: str = "must be an absolute path"
 
     class Config:
         """Configuration for class."""
@@ -134,12 +134,12 @@ class FTPConstants(BaseModel):
     """Constants related to FTP configuration.
 
     Attributes:
-        ftp_secret_username_env: str = Environment variable name for FTP username
-        ftp_secret_password_env: str = Environment variable name for FTP password
+        FTP_SECRET_USERNAME_ENV: str = Environment variable name for FTP username
+        FTP_SECRET_PASSWORD_ENV: str = Environment variable name for FTP password
     """
 
-    ftp_secret_username_env: str = "TESK_FTP_USERNAME"
-    ftp_secret_password_env: str = "TESK_FTP_PASSWORD"
+    FTP_SECRET_USERNAME_ENV: str = "TESK_FTP_USERNAME"
+    FTP_SECRET_PASSWORD_ENV: str = "TESK_FTP_PASSWORD"
 
     class Config:
         """Configuration for class."""
@@ -151,10 +151,10 @@ class PatchConstants(BaseModel):
     """Constants related to patch operations.
 
     Attributes:
-        cancel_path: str = Patch object passed to job API, when cancelling task
+        CANCEL_PATH: str = Patch object passed to job API, when cancelling task
     """
 
-    cancel_patch: str = '{"metadata":{"labels":{"task-status":"Cancelled"}}}'
+    CANCEL_PATCH: str = '{"metadata":{"labels":{"task-status":"Cancelled"}}}'
 
     class Config:
         """Configuration for class."""
@@ -166,22 +166,22 @@ class K8sConstants(BaseModel):
     """Constants related to Kubernetes.
 
     Attribute:
-        k8s_batch_api_version: Kubernetes Batch API version
-        k8s_batch_api_job_type: Kubernetes Job object type
-        job_restart_policy: Kubernetes Job restart policy
-        resource_cpu_key: Executor CPU resource label
-        resource_mem_key: Executor memory resource label
-        resource_mem_unit: Executor memory resource unit
-        resource_mem_one_gb: One Gibibyte (Gi) in bytes
+        K8S_BATCH_API_VERSION: Kubernetes Batch API version
+        K8S_BATCH_API_JOB_TYPE: Kubernetes Job object type
+        JOB_RESTART_POLICY: Kubernetes Job restart policy
+        RESOURCE_CPU_KEY: Executor CPU resource label
+        RESOURCE_MEM_KEY: Executor memory resource label
+        RESOURCE_MEM_UNIT: Executor memory resource unit
+        RESOURCE_MEM_ONE_GB: One Gibibyte (Gi) in bytes
     """
 
-    k8s_batch_api_version: str = "batch/v1"
-    k8s_batch_api_job_type: str = "Job"
-    job_restart_policy: str = "Never"
-    resource_cpu_key: str = "cpu"
-    resource_mem_key: str = "memory"
-    resource_mem_unit: str = "Gi"
-    resource_mem_one_gb: int = 1073741824
+    K8S_BATCH_API_VERSION: str = "batch/v1"
+    K8S_BATCH_API_JOB_TYPE: str = "Job"
+    JOB_RESTART_POLICY: str = "Never"
+    RESOURCE_CPU_KEY: str = "cpu"
+    RESOURCE_MEM_KEY: str = "memory"
+    RESOURCE_MEM_UNIT: str = "Gi"
+    RESOURCE_MEM_ONE_GB: int = 1073741824
 
     class Config:
         """Configuration for class."""
@@ -211,22 +211,22 @@ class TeskK8sConstants(BaseModel):
     """All the constants related to k8s and job creation.
 
     Attributes:
-        job_constants
-        annotation_constants
-        label_constants
-        path_validation_constants
-        ftp_constants
-        patch_constants
-        k8s_constants
+        JOB_CONSTANTS
+        ANNOTATION_CONSTANTS
+        LABEL_CONSTANTS
+        PATH_VALIDATION_CONSTANTS
+        FTP_CONSTANTS
+        PATCH_CONSTANTS
+        K8S_CONSTANTS
     """
 
-    job_constants: JobConstants = JobConstants()
-    annotation_constants: AnnotationConstants = AnnotationConstants()
-    label_constants: LabelConstants = LabelConstants()
-    path_validation_constants: PathValidationConstants = PathValidationConstants()
-    ftp_constants: FTPConstants = FTPConstants()
-    patch_constants: PatchConstants = PatchConstants()
-    k8s_constants: K8sConstants = K8sConstants()
+    JOB_CONSTANTS: JobConstants = JobConstants()
+    ANNOTATION_CONSTANTS: AnnotationConstants = AnnotationConstants()
+    LABEL_CONSTANTS: LabelConstants = LabelConstants()
+    PATH_VALIDATION_CONSTANTS: PathValidationConstants = PathValidationConstants()
+    FTP_CONSTANTS: FTPConstants = FTPConstants()
+    PATCH_CONSTANTS: PatchConstants = PatchConstants()
+    K8S_CONSTANTS: K8sConstants = K8sConstants()
 
     class Config:
         """Configuration for class."""
--- tesk/constants.py
+++ tesk/constants.py
@@ -9,16 +9,16 @@ class TeskConstants(BaseModel):
     """Tesk's K8s scoped constants.
 
     Attributes:
-        filer_image_name: Name of the filer image
-        filer_image_version: Version of the filer image
-        taskmaster_image_name:  Name of the taskmaster image
-        taskmaster_image_version: Version of the taskmaster image
-        tesk_namespace: Namespace in which api will create K8s resources from TES
+        FILER_IMAGE_NAME: Name of the filer image
+        FILER_IMAGE_VERSION: Version of the filer image
+        TASKMASTER_IMAGE_NAME:  Name of the taskmaster image
+        TASKMASTER_IMAGE_VERSION: Version of the taskmaster image
+        TESK_NAMESPACE: Namespace in which api will create K8s resources from TES
             request
-        taskmaster_service_account_name: Taskmaster service account name
-        taskmaster_environment_executor_backoff_limit:  Backoff limit for taskmaster env
-        filer_backoff_limit: Backoff limit got filer job
-        executor_backoff_limit: Backoff limit for executor job
+        TASKMASTER_SERVICE_ACCOUNT_NAME: Taskmaster service account name
+        TASKMASTER_ENVIRONMENT_EXECUTOR_BACKOFF_LIMIT:  Backoff limit for taskmaster env
+        FILER_BACKOFF_LIMIT: Backoff limit got filer job
+        EXECUTOR_BACKOFF_LIMIT: Backoff limit for executor job
 
     Note:
         Below are the mentioned environment variable with which these constants can be
@@ -27,49 +27,51 @@ class TeskConstants(BaseModel):
         variable:
             ENV_VARIABLE = default
 
-        filer_image_name:
+        FILER_IMAGE_NAME:
             TESK_API_TASKMASTER_FILER_IMAGE_NAME = docker.io/elixircloud/tesk-core-filer
-        filer_image_version:
+        FILER_IMAGE_VERSION:
             TESK_API_TASKMASTER_FILER_IMAGE_VERSION = latest
-        taskmaster_image_name:
+        TASKMASTER_IMAGE_NAME:
             TESK_API_TASKMASTER_IMAGE_NAME = docker.io/elixircloud/tesk-core-taskmaster
-        taskmaster_image_version:
+        TASKMASTER_IMAGE_VERSION:
             TESK_API_TASKMASTER_IMAGE_VERSION = latest
-        tesk_namespace:
+        TESK_NAMESPACE:
             TESK_API_K8S_NAMESPACE = tesk
-        taskmaster_service_account_name:
+        TASKMASTER_SERVICE_ACCOUNT_NAME:
             TESK_API_TASKMASTER_SERVICE_ACCOUNT_NAME = taskmaster
-        taskmaster_environment_executor_backoff_limit:
+        TASKMASTER_ENVIRONMENT_EXECUTOR_BACKOFF_LIMIT:
             ENVIRONMENT_EXECUTOR_BACKOFF_LIMIT = 6
-        filer_backoff_limit:
+        FILER_BACKOFF_LIMIT:
             FILER_BACKOFF_LIMIT = 2
-        executor_backoff_limit:
+        EXECUTOR_BACKOFF_LIMIT:
             EXECUTOR_BACKOFF_LIMIT = 2
     """
 
-    filer_image_name: str = os.getenv(
+    FILER_IMAGE_NAME: str = os.getenv(
         "TESK_API_TASKMASTER_FILER_IMAGE_NAME", "docker.io/elixircloud/tesk-core-filer"
     )
-    filer_image_version: str = os.getenv(
+    FILER_IMAGE_VERSION: str = os.getenv(
         "TESK_API_TASKMASTER_FILER_IMAGE_VERSION", "latest"
     )
-    taskmaster_image_name: str = os.getenv(
+    TASKMASTER_IMAGE_NAME: str = os.getenv(
         "TESK_API_TASKMASTER_IMAGE_NAME", "docker.io/elixircloud/tesk-core-taskmaster"
     )
-    taskmaster_image_version: str = os.getenv(
+    TASKMASTER_IMAGE_VERSION: str = os.getenv(
         "TESK_API_TASKMASTER_IMAGE_VERSION", "latest"
     )
-    tesk_namespace: str = os.getenv("TESK_API_K8S_NAMESPACE", "tesk")
-    taskmaster_service_account_name: str = os.getenv(
+    TESK_NAMESPACE: str = os.getenv("TESK_API_K8S_NAMESPACE", "tesk")
+    TASKMASTER_SERVICE_ACCOUNT_NAME: str = os.getenv(
         "TESK_API_TASKMASTER_SERVICE_ACCOUNT_NAME", "taskmaster"
     )
-    taskmaster_environment_executor_backoff_limit: str = os.getenv(
+    TASKMASTER_ENVIRONMENT_EXECUTOR_BACKOFF_LIMIT: str = os.getenv(
         "ENVIRONMENT_EXECUTOR_BACKOFF_LIMIT", "6"
     )
-    filer_backoff_limit: str = os.getenv("FILER_BACKOFF_LIMIT", "2")
-    executor_backoff_limit: str = os.getenv("EXECUTOR_BACKOFF_LIMIT", "2")
+    FILER_BACKOFF_LIMIT: str = os.getenv("FILER_BACKOFF_LIMIT", "2")
+    EXECUTOR_BACKOFF_LIMIT: str = os.getenv("EXECUTOR_BACKOFF_LIMIT", "2")
 
     class Config:
         """Configuration for class."""
 
         frozen = True
+
+tesk_constants = TeskConstants()
